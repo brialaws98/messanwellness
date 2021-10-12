dc43b6d9e767307c3acc17e83477d8611c02a8ee
brialaws98 <brianalaws98@gmail.com>
2021-10-12 12:14:33 -0400

    Map out the home page
    This will help when I start making the components for each section.

---
+++ src/home.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+ export default function HomePage () {
+     return (
+    <div>
+        <h1 class="text-center">
+            About Messan Wellness 
+            <hr class="topline" width="42px" /> 
+        </h1>
+            <p class="text-center">
+            Healing, Evolution, and Transformation is nurtured at 
+            Mesan Wellness. Established in 2018 by Morine Aneesah Lewis. 
+            Our Wellness Services support the overall well-being of the 
+            Spirit, Mind, and Body. <em>*Reiki, Hypnosis, and Coaching can be 
+            provided both virtual or in person.* </em>
+            </p>
+
+         <hr/>
+         <h4>
+           **Show different services**
+                 *Use Flexbox*
+         </h4>
+
+         <hr/>
+         <h2>Blog</h2>
+         <p>
+         Let's discuss health and wellness together
+         </p>
+         <hr/>
+            **Testimonial**
+               *bacground image*
+
+         <hr/>
+         **Contact**
+    </div>
+        );
+ }
