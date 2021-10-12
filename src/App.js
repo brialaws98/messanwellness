ab66d948131c6a7208bd73fb9e93d916d1521cd9
brialaws98 <brianalaws98@gmail.com>
2021-10-12 12:13:01 -0400

    Add a home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import NavBar  from "./navbar.js";
 import Footer from "./footer";
+import HomePage from "./home";
 
 function App() {
     return (
         <div>
             <NavBar />
-            
+            <HomePage />
             <Footer />
         </div>
     );
