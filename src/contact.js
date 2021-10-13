80ccbc6c63df2d3186ee906a37f2c81e4d20bfdf
brialaws98 <brianalaws98@gmail.com>
2021-10-13 12:40:50 -0400

    Add form for contact
    Users will be able to send a message with this feature.

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,43 @@ import React from "react";
 
  export default function ContactUs () {
      return (
-    <div>
-         **Contact **
-    </div> 
+    <div className="contactMe">
+     <div>
+              <h2>Contact Us</h2>
+         <br/>
+         <p><strong>Location: 516 N Rolling Road Ste 305Catonsville,
+               Maryland
+               <br />
+               Ph: (443) 297-9731
+
+         </strong></p>
+     </div>
+     <div>
+          <form>
+               <label>
+                    <input
+                      type="text" 
+                      placeholder="Name"
+                    />
+                    <input
+                       type="text"
+                       placeholder="Email"
+                    />
+                    <input
+                       type="text"
+                       placeholder="subject"
+                    />
+                    <input
+                       type="text"
+                       placeholder="type your message here..."
+                       width="20px"
+                    />
+               
+               </label>
+          </form>
+     </div>
+     
+    </div>
 
      );
 }
