c5db6925ececb952d3dfa24c3cb8f0839f9471a5
brialaws98 <brianalaws98@gmail.com>
2021-10-13 10:28:18 -0400

    Put image folder into the public folder
    In order to the images to show on the web page, I had to put the "img" folder in the "public" folder.

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import "./Navbar.css";
      return (
         <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-    <img src="img/logo.webp" alt=" company logo" />
+    <img src="./img/logo.webp" alt=" company logo" />
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
