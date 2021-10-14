29f5ebcf7bba4a9b097a37d28ce321d268899219
brialaws98 <brianalaws98@gmail.com>
2021-10-14 15:49:26 -0400

    Fix the image
    Tried to edit the photos

--- src/services.js
+++ src/services.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React from "react"
+import "./Service.css";
 
  export default function Services () {
      return(
-        <p>
-            <img src="./img/massage_book.webp" alt="" />
-            <img src="./img/reiki_book.webp" alt="" />
-            <img src="./img/hypnosis_book.webp" alt="" />
-            <img src="./img/spiritual_life_coach_book.webp" alt="" />
-        </p>
+ <div className="container">
+     <div className="row">
+         <div className="col-3">
+           <img src="./img/massage_book.webp" alt="" width="200px" />
+           <img src="./img/reiki_book.webp" alt=""  />
+        </div>
+         
+         <div className="col-3">
+             <img src="./img/hypnosis_book.webp" alt="" />
+           <img src="./img/spiritual_life_coach_book.webp" alt=""  />
+         </div>
+           
+     </div>                 
+</div>
      );
  }
