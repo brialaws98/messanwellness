bb377141e6a5d9b4ba1c421c48be5bf3522eaf2b
brialaws98 <brianalaws98@gmail.com>
2021-10-15 01:20:20 -0400

    Re-fixed position and size of cards
    The photos and description is centered.

--- src/services.js
+++ src/services.js
@@ -10,7 +10,7 @@ import "./Service.css";
             <img src="./img/massage_book.webp" alt="" />
   <div class="card-body">
     <h5 class="card-title"> Massage</h5>
-    <p class="card-text">Specializing in Deep Tissue, Swedish or Hot Stone therapy. We provide custom massage treatments so that you can take charge of your health and well-being. </p>
+    <p class="text-center">Specializing in Deep Tissue, Swedish or Hot Stone therapy. We provide custom massage treatments so that you can take charge of your health and well-being. </p>
     <a href="#" class="btn btn-primary">Book Now</a>
   </div>
 </div>
@@ -20,7 +20,7 @@ import "./Service.css";
    <img src="./img/reiki_book.webp" alt="" />
   <div class="card-body">
     <h5 class="card-title">Reiki</h5>
-    <p class="card-text">
+    <p class="text-center">
 Reiki
 
 Book Now
@@ -37,7 +37,7 @@ Pronounced (Ray-Key) is an energy-based healing technique used to activate the n
    <img src="./img/hypnosis_book.webp" alt="" />
   <div class="card-body">
     <h5 class="card-title">Hypnosis</h5>
-    <p class="card-text">Through hypnosis we use your most powerful resource - your mind - to help you overcome obstacles that prevent you from becoming the absolute best you can be. Specializing in Past Life Regression and future life progression. </p>
+    <p class="text-center">Through hypnosis we use your most powerful resource - your mind - to help you overcome obstacles that prevent you from becoming the absolute best you can be. Specializing in Past Life Regression and future life progression. </p>
     <a href="#" class="btn btn-primary">Book Now</a>
   </div>
 </div>
@@ -47,7 +47,7 @@ Pronounced (Ray-Key) is an energy-based healing technique used to activate the n
    <img src="./img/spiritual_life_coach_book.webp" alt="" />
   <div class="card-body">
     <h5 class="card-title">Spiritual Life Coaching  </h5>
-    <p class="card-text">This unique program eloquently incorporates practical life coaching strategies while integrating spirituality, the soul, spirit, and energy that goes beyond the physical. It empowers people to awaken their unique spiritual soul gifts and engages the deeper driving forces of living on purpose.</p>
+    <p class="text-center">This unique program eloquently incorporates practical life coaching strategies while integrating spirituality, the soul, spirit, and energy that goes beyond the physical. It empowers people to awaken their unique spiritual soul gifts and engages the deeper driving forces of living on purpose.</p>
     <a href="#" class="btn btn-primary">Book Now</a>
   </div>
 </div>
