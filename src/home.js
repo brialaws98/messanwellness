6b40c600ed8f32290cda26e549a5363ac8c62487
brialaws98 <brianalaws98@gmail.com>
2021-10-15 01:03:47 -0400

    Edit name of a container

--- src/home.js
+++ src/home.js
@@ -3,11 +3,13 @@ import Services from "./services";
 import Blog from "./blog";
 import Testimonial from "./testimonials";
 import ContactUs from "./contact";
+import "./App.css";
 
  export default function HomePage () {
      return (
     <div>
-        <h1 class="text-center">
+      <div className="aboutMe">
+           <h1 class="text-center">
             About Messan Wellness 
             <hr class="topline" width="42px" /> 
         </h1>
@@ -18,6 +20,8 @@ import ContactUs from "./contact";
             Spirit, Mind, and Body. <em>*Reiki, Hypnosis, and Coaching can be 
             provided both virtual or in person.* </em>
             </p>
+      </div>
+       
 
          <hr/>
         
