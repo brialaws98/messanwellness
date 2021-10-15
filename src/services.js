4eeb2e80a9fea2ab05ab327683eb24ee4e1d63f9
brialaws98 <brianalaws98@gmail.com>
2021-10-14 21:10:53 -0400

    Fix the photos
    I use the cards from bootstrap to help with the design services and the description. Need to fix the spacing of each card.

--- src/services.js
+++ src/services.js
@@ -3,19 +3,56 @@ import "./Service.css";
 
  export default function Services () {
      return(
- <div className="container">
-     <div className="row">
-         <div className="col-3">
-           <img src="./img/massage_book.webp" alt="" width="200px" />
-           <img src="./img/reiki_book.webp" alt=""  />
-        </div>
-         
-         <div className="col-3">
-             <img src="./img/hypnosis_book.webp" alt="" />
-           <img src="./img/spiritual_life_coach_book.webp" alt=""  />
-         </div>
-           
-     </div>                 
+        <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div class="card" >
+            <img src="./img/massage_book.webp" alt="" />
+  <div class="card-body">
+    <h5 class="card-title"> Massage</h5>
+    <p class="card-text">Specializing in Deep Tissue, Swedish or Hot Stone therapy. We provide custom massage treatments so that you can take charge of your health and well-being. </p>
+    <a href="#" class="btn btn-primary">Book Now</a>
+  </div>
 </div>
+          </div>
+          <div class="col-6">
+            <div class="card" >
+   <img src="./img/reiki_book.webp" alt="" />
+  <div class="card-body">
+    <h5 class="card-title">Reiki</h5>
+    <p class="card-text">
+Reiki
+
+Book Now
+Pronounced (Ray-Key) is an energy-based healing technique used to activate the natural healing processes of the body and restore physical and emotional well-being.</p>
+    <a href="#" class="btn btn-primary">Book Now</a>
+  </div>
+</div>
+          </div>
+      
+          <div class="w-100"></div>
+      
+          <div class="col-6">
+              <div class="card" >
+   <img src="./img/hypnosis_book.webp" alt="" />
+  <div class="card-body">
+    <h5 class="card-title">Hypnosis</h5>
+    <p class="card-text">Through hypnosis we use your most powerful resource - your mind - to help you overcome obstacles that prevent you from becoming the absolute best you can be. Specializing in Past Life Regression and future life progression. </p>
+    <a href="#" class="btn btn-primary">Book Now</a>
+  </div>
+</div>
+          </div>
+          <div class="col-6">
+              <div class="card" >
+   <img src="./img/spiritual_life_coach_book.webp" alt="" />
+  <div class="card-body">
+    <h5 class="card-title">Spiritual Life Coaching  </h5>
+    <p class="card-text">This unique program eloquently incorporates practical life coaching strategies while integrating spirituality, the soul, spirit, and energy that goes beyond the physical. It empowers people to awaken their unique spiritual soul gifts and engages the deeper driving forces of living on purpose.</p>
+    <a href="#" class="btn btn-primary">Book Now</a>
+  </div>
+</div>
+          </div>
+        </div>
+      </div>
      );
  }
