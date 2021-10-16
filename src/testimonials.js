4b59aa2c36c85b601443641c955914fc728e149c
brialaws98 <brianalaws98@gmail.com>
2021-10-16 18:09:07 -0400

    Edit the size of the rock image
    I made the width and height of the image bigger.

--- src/testimonials.js
+++ src/testimonials.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./testimonials.css";
 
  export default function Testimonial () {
      return (
          <div  class="bg_image"
          style={{
            backgroundImage: 'url("./img/testimonial.webp")',
-           backgroundSize: "cover",
-           width: "200px"
+           backgroundSize:"cover",
+           width:"1349px",
+           height: "628px"
+
      }}>
              **Testimonial page 
              *has background image
