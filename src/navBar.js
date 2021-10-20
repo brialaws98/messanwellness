96b3e60cfc7e248ce707c057ec7185db9c49c915
brialaws98 <brianalaws98@gmail.com>
2021-10-20 10:22:35 -0400

    Edit the "NavBar"
    I Change the color of the Navigation bar to match the original website.

--- src/navBar.js
+++ src/navBar.js
@@ -2,17 +2,24 @@ import React from "react";
 import "./Navbar.css";
 import * as ReactBootStrap from "react-bootstrap";
 
- export default function NavBar (){
+ export default function Navbar () {
      return (
-        <Navbar bg="dark" variant="dark">
-        
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>
-          <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-          <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-        </Nav>
-        
-      </Navbar>
+     <ReactBootStrap.Navbar  className="color-nav" expand="lg">
+
+       <ReactBootStrap.Navbar.Brand href="#home">
+          <img src="./img/logo.webp" />
+        </ReactBootStrap.Navbar.Brand>
+       <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+       <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+          <ReactBootStrap.Nav className="me-auto">
+            <ReactBootStrap.Nav.Link href="#home">
+              Home
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#link">
+              Link
+            </ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
      );
  }
