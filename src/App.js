a18d52d430539b295ad32c1fc89d2112ed3b759e
brialaws98 <brianalaws98@gmail.com>
2021-10-22 21:57:39 -0400

    Edit the home screen
    This should allow the home screen to be the first page that shows up.

--- src/App.js
+++ src/App.js
@@ -25,25 +25,23 @@ function App() {
             <NavBar />
 
             <Switch>
-          <Route exact path="/home" component={HomePage}>
-            <HomePage />
-            <Redirect to="/home" />
-          </Route>
+          <Route  path="/home" exact component={HomePage} />
+            
   
 
           <Route  path="/blog" component={BlogPage} />
 
-          <Route  exact path="/contact" component={ContactUs}/>
+          <Route   path="/contact" component={ContactUs}/>
             
 
-          <Route exact path="/listservice" component={OurService} />
+          <Route  path="/listservice" component={OurService} />
 
-          <Route exact path="#" component={Services} />
+          <Route  path="#" component={Services} />
 
 
-          <Route  eaxct path="/testimonials" component={Testimonial}/>
+          <Route path="/testimonials" component={Testimonial}/>
 
-          <Route exact path="/services" component={OurService} />
+          <Route  path="/services" component={OurService} />
 
           </Switch>
           
